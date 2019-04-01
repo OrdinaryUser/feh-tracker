597fa166f415cf5d705bad0a278e027f01f3726e
Alex Holt <alex.holt131@gmail.com>
2019-03-31 21:29:10 -0400

    Routing and views.

---
+++ src/pages/ListHeroes.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="faux-modal main-modal">
+    <h1 class="page-title">
+      All Heroes
+    </h1>
+    <ul>
+      <li v-for="hero in heroes" v-bind:key="hero.id">
+        <router-link :to="{ name: '/ViewHero/:id', params: { id: hero.id } }">{{hero.name}}</router-link>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+  import {db} from '../db'
+  export default {
+    name: 'app',
+
+    data () {
+      return { heroes: [{name: 'NOT LOADING', id: '-1'}] }
+    },
+
+    firestore: {
+      heroes: db.collection('heroes')
+    }
+  }
+</script>
---
+++ src/pages/ViewHero.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="faux-modal main-modal">
+    <h1 class="page-title">
+      {{hero.name}}
+    </h1>
+  </div>
+</template>
+
+<script>
+  import {db} from '../db'
+  export default {
+    name: 'app',
+
+    data () {
+      return { hero: { name: 'NOT LOADING' } }
+    },
+
+    firestore: {
+      hero: db.collection('heroes')
+    }
+  }
+</script>
